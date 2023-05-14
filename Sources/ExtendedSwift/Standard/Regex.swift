09f0fa27708572c43716ee6619c2c50702df2760
Dave DeLong <davedelong@me.com>
2023-05-14 16:44:42 -0600

    better regex replacement APIs

--- Sources/ExtendedSwift/Standard/Regex.swift
+++ Sources/ExtendedSwift/Standard/Regex.swift
@@ -9,112 +9,187 @@ import Foundation
 
 extension Regex {
     
+    public func lastMatch(in s: String) throws -> Match? {
+        return try self.lastMatch(in: s[...])
+    }
+    
+    public func lastMatch(in s: Substring) throws -> Match? {
+        var previous: Match?
+        try enumerateMatches(in: s, using: { match, _ in
+            previous = match
+        })
+        return previous
+    }
+    
     public func allMatches(in s: String) throws -> Array<Match> {
         return try self.allMatches(in: s[...])
     }
     
     public func allMatches(in s: Substring) throws -> Array<Match> {
-        var remaining = s
         var matches = Array<Match>()
-        
+        try enumerateMatches(in: s, using: { match, _ in
+            matches.append(match)
+        })
+        return matches
+    }
+    
+    public func enumerateMatches(in s: String, using enumerator: (Match, inout Bool) -> Void) throws {
+        try self.enumerateMatches(in: s[...], using: enumerator)
+    }
+    
+    public func enumerateMatches(in s: Substring, using enumerator: (Match, inout Bool) -> Void) throws {
+        var remaining = s
         while let next = try self.firstMatch(in: remaining) {
-            matches.append(next)
+            var keepGoing = true
+            enumerator(next, &keepGoing)
             
             let endOfMatch = next.range.upperBound
             
-            if endOfMatch < remaining.endIndex {
+            if endOfMatch < remaining.endIndex && keepGoing == true {
                 remaining = remaining[endOfMatch ..< remaining.endIndex]
             } else {
                 break
             }
         }
-        
-        return matches
     }
     
 }
 
 extension String {
     
-    public func replaceOccurrences<Output>(of regex: Regex<Output>, with replacement: RegexReplacement<Output>) -> String {
-        return self.replacing(regex, with: { match in
-            return replacement.buildReplacement(match)
-        })
+    public func replacingFirstMatch<Output>(of regex: Regex<Output>, with replacement: Regex<Output>.Replacement) -> String {
+        guard let match = try? regex.firstMatch(in: self) else { return self }
+        let replacement = replacement.buildReplacement(match)
+        var copy = self
+        copy.replaceSubrange(match.range, with: replacement)
+        return copy
     }
     
-    public func replaceOccurrences<Output>(of regex: Regex<Output>, @RegexReplacementBuilder<Output> with builder: () -> [RegexReplacementPart<Output>] ) -> String {
-        let replacement = RegexReplacement(parts: builder())
-        return self.replaceOccurrences(of: regex, with: replacement)
+    public func replacingFirstMatch<Output>(of regex: Regex<Output>, @Regex<Output>.ReplacementBuilder with builder: () -> [Regex<Output>.ReplacementPart]) -> String {
+        let replacement = Regex.Replacement(parts: builder())
+        return self.replacingFirstMatch(of: regex, with: replacement)
+    }
+    
+    public func replacingLastMatch<Output>(of regex: Regex<Output>, with replacement: Regex<Output>.Replacement) -> String {
+        guard let match = try? regex.lastMatch(in: self) else { return self }
+        let replacement = replacement.buildReplacement(match)
+        var copy = self
+        copy.replaceSubrange(match.range, with: replacement)
+        return copy
+    }
+    
+    public func replacingLastMatch<Output>(of regex: Regex<Output>, @Regex<Output>.ReplacementBuilder with builder: () -> [Regex<Output>.ReplacementPart]) -> String {
+        let replacement = Regex.Replacement(parts: builder())
+        return self.replacingLastMatch(of: regex, with: replacement)
+    }
+    
+    public func replacingAllMatches<Output>(of regex: Regex<Output>, with replacement: Regex<Output>.Replacement) -> String {
+        return self.replacing(regex, with: { replacement.buildReplacement($0) })
+    }
+    
+    public func replacingAllMatches<Output>(of regex: Regex<Output>, @Regex<Output>.ReplacementBuilder with builder: () -> [Regex<Output>.ReplacementPart]) -> String {
+        let replacement = Regex.Replacement(parts: builder())
+        return self.replacingAllMatches(of: regex, with: replacement)
+    }
+    
+    // MARK: - Mutating Versions
+    
+    public mutating func replaceFirstMatch<Output>(of regex: Regex<Output>, with replacement: Regex<Output>.Replacement) {
+        self = self.replacingFirstMatch(of: regex, with: replacement)
+    }
+    
+    public mutating func replaceFirstMatch<Output>(of regex: Regex<Output>, @Regex<Output>.ReplacementBuilder with builder: () -> [Regex<Output>.ReplacementPart]) {
+        let replacement = Regex.Replacement(parts: builder())
+        self.replaceFirstMatch(of: regex, with: replacement)
+    }
+    
+    public mutating func replaceLastMatch<Output>(of regex: Regex<Output>, with replacement: Regex<Output>.Replacement) {
+        self = self.replacingLastMatch(of: regex, with: replacement)
+    }
+    
+    public mutating func replaceLastMatch<Output>(of regex: Regex<Output>, @Regex<Output>.ReplacementBuilder with builder: () -> [Regex<Output>.ReplacementPart]) {
+        let replacement = Regex.Replacement(parts: builder())
+        self.replaceLastMatch(of: regex, with: replacement)
+    }
+    
+    public mutating func replaceAllMatches<Output>(of regex: Regex<Output>, with replacement: Regex<Output>.Replacement) {
+        self = self.replacingAllMatches(of: regex, with: replacement)
+    }
+    
+    public mutating func replaceAllMatches<Output>(of regex: Regex<Output>, @Regex<Output>.ReplacementBuilder with builder: () -> [Regex<Output>.ReplacementPart]) {
+        let replacement = Regex.Replacement(parts: builder())
+        self.replaceAllMatches(of: regex, with: replacement)
     }
     
 }
 
-public enum RegexReplacementPart<Output> {
-    case literal(String)
-    case matchPortion(KeyPath<Regex<Output>.Match, Substring>)
-}
-
-public struct RegexReplacement<Output>: ExpressibleByStringInterpolation {
+extension Regex {
     
-    public struct StringInterpolation: StringInterpolationProtocol {
-        
-        internal var parts = Array<RegexReplacementPart<Output>>()
-        
-        public init(literalCapacity: Int, interpolationCount: Int) { }
-        
-        public mutating func appendLiteral(_ literal: String) {
-            parts.append(.literal(literal))
-        }
-        
-        public mutating func appendInterpolation(_ keyPath: KeyPath<Regex<Output>.Match, Substring>) {
-            parts.append(.matchPortion(keyPath))
-        }
+    public enum ReplacementPart {
+        case literal(String)
+        case matchPortion(KeyPath<Regex<Output>.Match, Substring>)
     }
     
-    private let parts: Array<RegexReplacementPart<Output>>
-    
-    internal init(parts: Array<RegexReplacementPart<Output>>) {
-        self.parts = parts
-    }
-    
-    public init(stringLiteral value: StringLiteralType) {
-        parts = [.literal(value)]
-    }
-    
-    public init(stringInterpolation: StringInterpolation) {
-        parts = stringInterpolation.parts
-    }
-    
-    internal func buildReplacement(_ match: Regex<Output>.Match) -> String {
-        var final = ""
-        for part in parts {
-            switch part {
-                case .literal(let s): final.append(s)
-                case .matchPortion(let kp): final.append(contentsOf: match[keyPath: kp])
+    public struct Replacement: ExpressibleByStringInterpolation {
+        
+        public struct StringInterpolation: StringInterpolationProtocol {
+            
+            internal var parts = Array<ReplacementPart>()
+            
+            public init(literalCapacity: Int, interpolationCount: Int) { }
+            
+            public mutating func appendLiteral(_ literal: String) {
+                parts.append(.literal(literal))
+            }
+            
+            public mutating func appendInterpolation(_ keyPath: KeyPath<Regex<Output>.Match, Substring>) {
+                parts.append(.matchPortion(keyPath))
             }
         }
-        return final
+        
+        private let parts: Array<ReplacementPart>
+        
+        internal init(parts: Array<ReplacementPart>) {
+            self.parts = parts
+        }
+        
+        public init(stringLiteral value: StringLiteralType) {
+            parts = [.literal(value)]
+        }
+        
+        public init(stringInterpolation: StringInterpolation) {
+            parts = stringInterpolation.parts
+        }
+        
+        internal func buildReplacement(_ match: Regex<Output>.Match) -> String {
+            var final = ""
+            for part in parts {
+                switch part {
+                    case .literal(let s): final.append(s)
+                    case .matchPortion(let kp): final.append(contentsOf: match[keyPath: kp])
+                }
+            }
+            return final
+        }
     }
     
-}
-
-@resultBuilder
-public enum RegexReplacementBuilder<Output> {
-    public typealias Part = RegexReplacementPart<Output>
-    
-    public static func buildExpression(_ expression: String) -> Part {
-        return .literal(expression)
+    @resultBuilder
+    public enum ReplacementBuilder {
+        public static func buildExpression(_ expression: String) -> ReplacementPart {
+            return .literal(expression)
+        }
+        
+        public static func buildExpression(_ expression: KeyPath<Regex<Output>.Match, Substring>) -> ReplacementPart {
+            return .matchPortion(expression)
+        }
+        
+        public static func buildArray(_ components: [[ReplacementPart]]) -> [ReplacementPart] {
+            return components.flattened()
+        }
+        
+        public static func buildBlock(_ components: ReplacementPart...) -> [ReplacementPart] {
+            return components
+        }
     }
     
-    public static func buildExpression(_ expression: KeyPath<Regex<Output>.Match, Substring>) -> Part {
-        return .matchPortion(expression)
-    }
-    
-    public static func buildArray(_ components: [[Part]]) -> [Part] {
-        return components.flattened()
-    }
-    
-    public static func buildBlock(_ components: Part...) -> [Part] {
-        return components
-    }
 }
--- Tests/ExtendedSwiftTests/RegexTests.swift
+++ Tests/ExtendedSwiftTests/RegexTests.swift
@@ -10,6 +10,15 @@ import ExtendedSwift
 
 class RegexTests: XCTestCase {
     
+    func testLastMatch() throws {
+        let r = /(\d+)/
+        if let m1 = try r.lastMatch(in: "123 456") {
+            XCTAssertEqual(m1.1, "456")
+        } else {
+            XCTFail()
+        }
+    }
+    
     func testAllMatches() throws {
         let r = /\d+/
         
@@ -20,26 +29,38 @@ class RegexTests: XCTestCase {
         XCTAssertEqual(m2.count, 0)
     }
     
+    func testFirstReplacement() throws {
+        let r = /\d+/
+        let s1 = "123 456".replacingFirstMatch(of: r, with: "bob")
+        XCTAssertEqual(s1, "bob 456")
+    }
+    
+    func testLastReplacement() throws {
+        let r = /\d+/
+        let s1 = "123 456".replacingLastMatch(of: r, with: "bob")
+        XCTAssertEqual(s1, "123 bob")
+    }
+    
     func testReplacement() throws {
         let r1 = /(\d+)/
-        let s1 = "bob123".replaceOccurrences(of: r1, with: "")
+        let s1 = "bob123".replacingAllMatches(of: r1, with: "")
         XCTAssertEqual(s1, "bob")
         
-        let s2 = "bob".replaceOccurrences(of: r1, with: "")
+        let s2 = "bob".replacingAllMatches(of: r1, with: "")
         XCTAssertEqual(s2, "bob")
     }
     
     func testReplacementInterpolation() throws {
         let r1 = /(\d+)/
-        let s1 = "bob123".replaceOccurrences(of: r1, with: " \(\.1)")
+        let s1 = "bob123".replacingAllMatches(of: r1, with: " \(\.1)")
         XCTAssertEqual(s1, "bob 123")
-        let s2 = "bob123-456".replaceOccurrences(of: r1, with: " \(\.1)")
+        let s2 = "bob123-456".replacingAllMatches(of: r1, with: " \(\.1)")
         XCTAssertEqual(s2, "bob 123- 456")
     }
     
     func testReplacementBuilder() throws {
         let r1 = /(\d+)/
-        let s1 = "bob123".replaceOccurrences(of: r1, with: {
+        let s1 = "bob123".replacingAllMatches(of: r1, with: {
             " "
             \.1
         })
