c69af1c991282a825417f43ae2b4db034146ccca
Dave DeLong <davedelong@me.com>
2023-10-07 10:20:27 -0600

    AnyError

--- Sources/ExtendedSwift/Standard/Error.swift
+++ Sources/ExtendedSwift/Standard/Error.swift
@@ -7,6 +7,36 @@
 
 import Foundation
 
+public struct AnyError: Error, CustomStringConvertible {
+    
+    public let function: StaticString
+    public let fileID: StaticString
+    public let line: UInt
+    public let description: String
+    public let underlyingError: Error?
+    
+    public init(_ description: String, function: StaticString = #function, fileID: StaticString = #fileID, line: UInt = #line) {
+        self.function = function
+        self.fileID = fileID
+        self.line = line
+        self.description = description
+        self.underlyingError = nil
+    }
+    
+    public init(_ other: Error, function: StaticString = #function, fileID: StaticString = #fileID, line: UInt = #line) {
+        if let any = other as? AnyError {
+            self = any
+        } else {
+            self.function = function
+            self.fileID = fileID
+            self.line = line
+            self.description = other.localizedDescription
+            self.underlyingError = other
+        }
+    }
+    
+}
+
 public struct UnimplementedError: Error, CustomStringConvertible {
     
     public let function: StaticString
