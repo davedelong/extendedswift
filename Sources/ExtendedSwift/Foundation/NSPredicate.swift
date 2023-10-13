44b77b49e7f9045f4d3f16d8cc5ac9b05decfd70
Dave DeLong <davedelong@me.com>
2023-10-12 20:24:22 -0600

    Conform NSPredicate to Tree

---
+++ Sources/ExtendedSwift/Foundation/NSPredicate.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dave DeLong on 10/12/23.
+//
+
+import Foundation
+
+extension NSPredicate: Tree {
+    
+    public typealias Value = NSPredicate
+    
+    public var value: NSPredicate { self }
+    
+    public var children: Array<any Tree<NSPredicate>> {
+        if let compound = self as? NSCompoundPredicate {
+            return compound.subpredicates as! Array<NSPredicate>
+        }
+        return []
+    }
+    
+}
