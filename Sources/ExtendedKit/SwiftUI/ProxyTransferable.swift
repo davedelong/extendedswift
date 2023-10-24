fedf5ac32e7171f04671e0b0a52c4ff23e2d87f8
Dave DeLong <davedelong@me.com>
2023-10-24 08:29:27 -0600

    more availability?

--- Sources/ExtendedKit/SwiftUI/ProxyTransferable.swift
+++ Sources/ExtendedKit/SwiftUI/ProxyTransferable.swift
@@ -18,16 +18,19 @@ public protocol ProxyTransferable: Transferable where Representation == ProxyRep
 
 extension ProxyTransferable {
     
+    @available(iOS 16.1, macOS 13.0, tvOS 16.1, watchOS 9.1, *)
     public func transferableProxy() throws -> Proxy {
         throw UnimplementedError()
     }
     
+    @available(iOS 16.1, macOS 13.0, tvOS 16.1, watchOS 9.1, *)
     public func transferableProxy() async throws -> Proxy {
         // Needed because `try self.transferableProxy()` thinks its refering to the async version
         let method: () throws -> Proxy = self.transferableProxy
         return try method()
     }
     
+    @available(iOS 16.1, macOS 13.0, tvOS 16.1, watchOS 9.1, *)
     public static var transferRepresentation: ProxyRepresentation<Self, Proxy> {
         ProxyRepresentation(exporting: { simple in
             return try await simple.transferableProxy()
