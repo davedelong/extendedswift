a2b5003bf986e229ebe8d26fa3fa8f948c2db4ed
Dave DeLong <davedelong@me.com>
2025-01-26 09:47:42 -0700

    create data from hex string

--- Sources/ExtendedSwift/Standard/Data.swift
+++ Sources/ExtendedSwift/Standard/Data.swift
@@ -9,6 +9,16 @@ import Foundation
 
 extension Data {
     
+    public init?(hexString: String) {
+        guard hexString.allSatisfy(\.isHexDigit) else { return nil }
+        guard hexString.count.isMultiple(of: 2) else { return nil }
+        
+        let bytes = hexString.chunks(ofCount: 2).compactMap { UInt8($0, radix: 16) }
+        guard bytes.count == hexString.count / 2 else { return nil }
+        
+        self.init(bytes)
+    }
+    
     public var hexDescription: String {
         return self.map { String(format: "%02X", $0) }.joined()
     }
