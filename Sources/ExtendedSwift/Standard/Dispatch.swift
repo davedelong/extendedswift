a1e36a21dbfe78d7505bf17ed0ef84f02c1ce943
Dave DeLong <davedelong@me.com>
2025-11-17 13:49:04 -0700

    oops

--- Sources/ExtendedSwift/Standard/Dispatch.swift
+++ Sources/ExtendedSwift/Standard/Dispatch.swift
@@ -11,7 +11,7 @@ extension DispatchQueue {
     
     public func async(after duration: Duration, qos: DispatchQoS = .unspecified, flags: DispatchWorkItemFlags = [], execute work: @escaping () -> Void) {
         let components = duration.components
-        let nanoseconds = (UInt64(components.seconds) * UInt64(NSEC_PER_SEC)) + (UInt64(components.attoseconds) * UInt64(1e9))
+        let nanoseconds = (UInt64(components.seconds) * UInt64(NSEC_PER_SEC)) + (UInt64(components.attoseconds) / UInt64(1e9))
         let time = DispatchTime.now().advanced(by: .nanoseconds(Int(nanoseconds)))
         self.asyncAfter(deadline: time, qos: qos, flags: flags, execute: work)
     }
