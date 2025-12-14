ec7075951e09c62374a1c181079ac28e0c93ed65
Dave DeLong <davedelong@me.com>
2025-12-14 10:17:17 -0700

    collection+identifiable

---
+++ Sources/ExtendedSwift/Standard/Collection+Identifiable.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  ExtendedSwift
+//
+//  Created by Dave DeLong on 12/14/25.
+//
+
+import Foundation
+
+extension Collection where Element: Identifiable {
+    
+    public func firstIndex(with id: Element.ID) -> Index? {
+        return self.firstIndex(where: { $0.id == id })
+    }
+    
+    public func first(with id: Element.ID) -> Element? {
+        return self.first(where: { $0.id == id })
+    }
+    
+}
+
+extension BidirectionalCollection where Element: Identifiable {
+    
+    public func lastIndex(with id: Element.ID) -> Index? {
+        return self.lastIndex(where: { $0.id == id })
+    }
+    
+    public func last(with id: Element.ID) -> Element? {
+        return self.last(where: { $0.id == id })
+    }
+    
+}
