60ef5189c87feb2a0ae7df768001bfe726c06c67
Dave DeLong <davedelong@me.com>
2025-12-14 10:19:54 -0700

    permissive paramenter

--- Sources/ExtendedSwift/Standard/Collection+Identifiable.swift
+++ Sources/ExtendedSwift/Standard/Collection+Identifiable.swift
@@ -9,11 +9,13 @@ import Foundation
 
 extension Collection where Element: Identifiable {
     
-    public func firstIndex(with id: Element.ID) -> Index? {
+    public func firstIndex(with id: Element.ID?) -> Index? {
+        guard let id else { return nil }
         return self.firstIndex(where: { $0.id == id })
     }
     
-    public func first(with id: Element.ID) -> Element? {
+    public func first(with id: Element.ID?) -> Element? {
+        guard let id else { return nil }
         return self.first(where: { $0.id == id })
     }
     
@@ -21,11 +23,13 @@ extension Collection where Element: Identifiable {
 
 extension BidirectionalCollection where Element: Identifiable {
     
-    public func lastIndex(with id: Element.ID) -> Index? {
+    public func lastIndex(with id: Element.ID?) -> Index? {
+        guard let id else { return nil }
         return self.lastIndex(where: { $0.id == id })
     }
     
-    public func last(with id: Element.ID) -> Element? {
+    public func last(with id: Element.ID?) -> Element? {
+        guard let id else { return nil }
         return self.last(where: { $0.id == id })
     }
     
